f8f4f527abc9f00cf16e916e22f660d22d3bb984
Cari Costa <cari.costa90@gmail.com>
2022-08-13 16:01:22 -0300

    se agrega interaccion con DOM, archivo funciones.js temporalmente sin uso

--- js/funciones.js
+++ js/funciones.js
@@ -1,29 +1,3 @@
-//creo una clase para generar una plantilla de objeto para los productos
-
-class Producto {
-    constructor (codigo, nombre, precio){
-        this.codigo=codigo;
-        this.nombre=nombre;
-        this.precio=precio;        
-    }
-    sumaIva(){
-        this.precio=this.precio*1.22;
-    }
-}
-// agrego productos a un array, utilizo la misma informacion aplicada en el simulador, al agregar al proyecto implementare nombres de productos mas descriptivos
-const productos = [];
-productos.push(new Producto(1,"Producto 1 ", 100));
-productos.push(new Producto(2,"Producto 2", 120));
-productos.push(new Producto(3, "Producto 3", 500));
-productos.push(new Producto(4, "Producto 4", 350));
-productos.push(new Producto(5,"Producto 5", 260));
-
-// itero por el array aplicando el iva al precio de cada producto
-for(const prod of productos){
-    prod.sumaIva();
-}
-// console log para verificar el cambio en el precio de los productos posterior a la suma del iva
-console.log(productos);
 
 //creo una clase para generar una plantilla de objeto para los descuentos
 class Descuentos{
@@ -118,3 +92,26 @@ const mostrarTotalCarrito = () => {
     }
 }
 mostrarTotalCarrito();
+
+
+/* Que tengo que hacer con este carrito??
+1. * archivo stock.js
+2. lograr que en vez del alert, la seleccion de productos se tome desde el clic del boton agregar al carrito -- event listener + funcion agregar al
+    2.1 escuchar el click
+    2.2 agregar linea al carrito con los datos de la card (producto, precio)
+    2.3 el precio ya viene convertido a un valor numerico como para hacer calculos?
+3. que los elementos se sumen al carrito que esta en la pagina cart
+    3.1. agregar para editar cantidad?
+    3.2. Agregar para remover del carrito
+    3.3. agregar boton de seguir comprando?
+    3.4. verificar que se modifique el total con los cambios
+4. Generar boton en el carrito que haga el imput del descuento (lo mismo que en el primer punto, tiene que poder levantar la data desde el array de objetos)
+5. verificar que impacte el descuento seleccionado en el total y que agregue una row "descuento en el cart"
+
+los prompt deberian cambiarse por funciones que escuchen el dom (event listener?)
+los alert deberian pasar a modificar el dom, en este caso en el cart 
+*/
+// para esta entrega hay que generar un carrito embebido en la store -- modal js
+
+/* en algun momento podemos agregar un filtro lentes de ver/de sol */
+/* separar el array de stock en otro js - se tienen que llamar en orden, primero el stock y despues la logica - los scripts de JS estan al final del body */
---
+++ js/stock.js
@@ -0,0 +1,69 @@
+/* productos del carrito para cards  */
+
+/* 
+1. lograr que coincida la informacion del objeto productos con los elementos en la tienda
+    1.1 ver pagina 48 de ppt clase 8 explica como generar divs con la informacion de un array de productos.*/
+
+//creo una clase para generar una plantilla de objeto para los productos
+
+class Producto {
+    constructor (codigo, nombre, precio, descripcion, categoria, imagen){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.precio=precio;        
+        this.descripcion=descripcion;
+        this.categoria=categoria;
+        this.imagen=imagen;
+    }
+    // agrego el teFixed para que queden solo 2 decimales al calcular el iva
+    sumaIva(){
+        this.precio=parseFloat(this.precio*1.22).toFixed(2);
+    }
+}
+// agrego productos a un array 
+
+let productos = [];
+productos.push(new Producto(1,"Producto 1", 1500, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/s6.jpg'));
+productos.push(new Producto(2,"Producto 2", 2700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/sunglasses-on-yellow-beach-towel.png'));
+productos.push(new Producto(3,"Producto 3", 2200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/designed-sunglasses-on-the-red-book-and-grey-surface.png'));
+productos.push(new Producto(4,"Producto 4", 3150, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/front-view-modern-dark-sunglasses-on-the-orange-black.png'));
+productos.push(new Producto(5,"Producto 5", 2850, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '/images/eyeglasses-wear.png'));
+productos.push(new Producto(6,"Producto 6", 1700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '/images/optic-glasses-on-the-table.png'));
+productos.push(new Producto(7,"Producto 7", 2200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/m1.jpg'));
+productos.push(new Producto(8,"Producto 8", 3700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '/images/m2.jpg'));
+productos.push(new Producto(9,"Producto 9", 2450, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/m3.jpg'));
+productos.push(new Producto(10,"Producto 10", 1500, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/m4.jpg'));
+productos.push(new Producto(11,"Producto 11", 1900, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/s1.jpg'));
+productos.push(new Producto(12,"Producto 12", 2490, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/s2.jpg'));
+productos.push(new Producto(13,"Producto 13", 3200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/s3.jpg'));
+productos.push(new Producto(14,"Producto 14", 2300, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '/images/s4.jpg'));
+
+// itero por el array aplicando el iva al precio de cada producto
+for(const prod of productos){
+    prod.sumaIva();
+}    
+
+// obtener nodo de productos:
+
+const listadoProductos = document.getElementById('productos');
+
+// creo la funcion para generar las cards en el HTML interactuando con el DOM - lo hago distinto a lo visto en clase complementaria para practicar. En vez de forEach uso For...of
+
+function mostrarProductos(productos){
+    for (const prod of productos) {
+        let contenedor = document.createElement("div");
+        contenedor.className = 'card';
+        contenedor.style='width: 18rem;';
+        //Definimos el innerHTML del elemento con una plantilla de texto
+        contenedor.innerHTML = `<img src="${prod.imagen}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-title">${prod.nombre}</p>
+                                    <p class="card-text">${prod.descripcion}</p>
+                                    <p class="card-price">$${prod.precio}</p>
+                                    <a href="#" class="btn btn-light">Agregar al carrito</a>
+                                </div>  `;
+        listadoProductos.appendChild(contenedor);
+    }
+}
+
+mostrarProductos(productos);
