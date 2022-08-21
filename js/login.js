ee1b61be576318ab4021469b2d02df6ea29d5c2d
Cari Costa <cari.costa90@gmail.com>
2022-08-20 21:01:09 -0300

    mejoras en el login

--- js/login.js
+++ js/login.js
@@ -6,7 +6,6 @@ class Usuario{
 }
 const usuariosRegistrados = [];
 
-
 let btnRegistrar = document.getElementById("btnRegistrar");
 btnRegistrar.addEventListener('click', function(){
     //alert("funciona");
@@ -14,21 +13,34 @@ btnRegistrar.addEventListener('click', function(){
     //alert (emailIngresado);
     let passIngresada = document.getElementById("pass").value;
     //alert(passIngresada);
-    usuariosRegistrados.push(new Usuario(emailIngresado,passIngresada));    
-    GuardarUsuarios();
+    let existe;
+    // validar mail no registrado
+    // let existe = usuariosRegistrados.find(element => element.correo == emailIngresado);
+    if(usuariosRegistrados.length>0){
+        if(existe = usuariosRegistrados.find(element => element.correo == emailIngresado)){
+            alert('ya existe un usuario registrado con ese correo');    
+        }else{
+            usuariosRegistrados.push(new Usuario(emailIngresado,passIngresada));    
+            GuardarUsuarios();        }
+    }else{
+        usuariosRegistrados.push(new Usuario(emailIngresado,passIngresada));    
+        GuardarUsuarios();
+    }
 });
 
 let usuariosGuardados;
 let usuariosRecuperados;
 
 function GuardarUsuarios(){
-    usuariosGuardados = JSON.stringify(usuariosRegistrados);
-    localStorage.setItem('Lista de usuarios', usuariosGuardados);
-    alert('el usuario se ha guardado correctamente');
+        usuariosGuardados = JSON.stringify(usuariosRegistrados);
+        localStorage.setItem('Lista de usuarios', usuariosGuardados);
+        alert('el usuario se ha guardado correctamente');    
 }
 
 function RecuperarUsuarios() {
-    usuariosRecuperados = JSON.parse(usuariosGuardados);
+    //usuariosRecuperados = JSON.parse(usuariosGuardados);
+    //cambio esta linea porque no funcionaba si el usuario no se habia guardado en el mismo momento
+    usuariosRecuperados = JSON.parse(localStorage.getItem('Lista de usuarios'));
     console.log(usuariosRecuperados);
     console.log(usuariosGuardados);
 }
@@ -56,6 +68,8 @@ btnLoguear.addEventListener('click', function () {
     if(encontrarUsuario){
         if(encontrarUsuario.pass == passUsuarioLogin){
             alert('ingresa')
+            // window.location.assign("");
+            window.location.replace('../pages/store.html');
         }else{
             alert('pass incorrecta');
         }
@@ -64,3 +78,17 @@ btnLoguear.addEventListener('click', function () {
     }    
 });
 
+/* Posibles mejoras:
+1. borrar los campos input despues de crear un usuario *hecho*
+3. no poder crear usuario con mismo mail mas de 1 vez *hecho* 
+2. redirigir a la store cuando se ingrese exitosamente *hecho*
+
+
+4. limitantes en la pass, ej cantidad de caracteres, caracteres especiales
+5. que el mail sea un mail
+*/
+
+/* Pendientes: 
+1. revisar y aplicar la mejora de codigo que me paso Emiliano
+2. verificar si se puede agregar el sumar items desde el carrito con el ejemplo que me paso Emiliano
+*/
