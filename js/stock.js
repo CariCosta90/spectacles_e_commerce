79fb838d477a4d11e13722992d1740541b6a15dc
CariCosta <83260315+CariCosta90@users.noreply.github.com>
2022-09-06 14:35:14 -0300

    limpieza de codigo stock

--- js/stock.js
+++ js/stock.js
@@ -14,26 +14,9 @@ class Producto {
         this.precio=parseFloat(this.precio*1.22).toFixed(2);
     }
 }
-// agrego productos a un array 
-//let productos = [];
-
-// productos.push(new Producto(1,"Producto 1", 1500, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/s6.jpg'));
-// productos.push(new Producto(2,"Producto 2", 2700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/sunglasses-on-yellow-beach-towel.png'));
-// productos.push(new Producto(3,"Producto 3", 2200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/designed-sunglasses-on-the-red-book-and-grey-surface.png'));
-// productos.push(new Producto(4,"Producto 4", 3150, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/front-view-modern-dark-sunglasses-on-the-orange-black.png'));
-// productos.push(new Producto(5,"Producto 5", 2850, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '../images/eyeglasses-wear.png'));
-// productos.push(new Producto(6,"Producto 6", 1700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '../images/optic-glasses-on-the-table.png'));
-// productos.push(new Producto(7,"Producto 7", 2200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/m1.jpg'));
-// productos.push(new Producto(8,"Producto 8", 3700, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de prescripcion", '../images/m2.jpg'));
-// productos.push(new Producto(9,"Producto 9", 2450, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/m3.jpg'));
-// productos.push(new Producto(10,"Producto 10", 1500, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/m4.jpg'));
-// productos.push(new Producto(11,"Producto 11", 1900, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/s1.jpg'));
-// productos.push(new Producto(12,"Producto 12", 2490, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/s2.jpg'));
-// productos.push(new Producto(13,"Producto 13", 3200, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/s3.jpg'));
-// productos.push(new Producto(14,"Producto 14", 2300, "DESCRIPCIÓN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, culpa.", "lentes de Sol", '../images/s4.jpg'));
 
 let productos = [];
-console.log(productos);
+//implemento funcion async para hacer fetch de json con listado de productos. 
 const traerProductosJson = async () => {
     let response = await fetch ("../productos.json");
     let data = await response.json();
@@ -87,5 +70,5 @@ function mostrarProductos(productos){
     }
 }
 
-//mostrarProductos(productos);
+
 
